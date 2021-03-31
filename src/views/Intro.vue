4b657bd3f34c0b83ab5db47ca30b7369a8996faa
Rohit <nuanceinfotech.rohit@gmail.com>
2021-03-31 14:55:12 +0530

    31march mobile version changes

--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -3,63 +3,64 @@
 <div v-if="isMobile">
   <ion-page>
     <ion-content fullscreen="true">
-      <div id="container" class="main_self">
-      <ion-row>
-        <ion-col size="12">
-        <div class="drak_mode">
-          <img class="w-100" src="assets/images/dark_mode_logo.svg"/>
-        </div>
-          <div class="logo">
-            <img src="/assets/images/normal_u1.svg">
-          </div>
-        </ion-col>
-      </ion-row>
+      <div id="intro-page" class="main_self">
+        <div class="intro-conte">
+          <ion-row>
+            <ion-col size="12">
+              <div class="drak_mode">
+                <img class="logo-dark" src="assets/images/dark_mode_logo.svg"/>
+              </div>
+
+              <div class="logo">
+                <img src="/assets/images/normal_u1.svg">
+              </div>
+            </ion-col>
+          </ion-row>
       
-        <ion-slides pager="true">
-        <ion-slide>
-          <div class="slide">
-            <img src="/assets/images/normal_u61.svg"/>
-            <p>This app allows you to write letters & faxes easily</p>
-          </div>
-        </ion-slide>
+          <ion-slides pager="true">
+            <ion-slide>
+              <div class="slide">
+                <img src="/assets/images/normal_u61.svg"/>
+                <p>This app allows you to write letters & faxes easily</p>
+              </div>
+            </ion-slide>
 
-        <ion-slide>
-        <div class="slide">
-          <img src="/assets/images/normal_u71.svg"/>
-          <p>You don’t have to format nicely, we do that for you</p>
+            <ion-slide>
+              <div class="slide">
+                <img src="/assets/images/normal_u71.svg"/>
+                <p>You don’t have to format nicely, we do that for you</p>
+              </div>
+            </ion-slide>
+
+            <ion-slide>
+              <div class="slide">
+                <img src="/assets/images/normal_u83.svg"/>
+                <p>You don’t have to search and type addresses</p>
+              </div>
+            </ion-slide>
+
+            <ion-slide>
+              <div class="slide">
+                <img src="/assets/images/normal_u95.svg"/>
+                <p>You don’t have to print paper #green</p>
+              </div>
+            </ion-slide>
+
+            <ion-slide>
+              <div class="slide">
+                <img src="/assets/images/normal_u107.svg"/>
+                <p>You don’t have to buy envelope and stamps</p>
+              </div>
+            </ion-slide>
+
+          </ion-slides>
+
+          <ion-row>
+            <ion-col size="12">
+              <ion-button type="submit" shape="round" class="get_st" @click="() => router.push('/all-letters')">Get Started</ion-button>
+            </ion-col>
+          </ion-row>
         </div>
-        </ion-slide>
-
-        <ion-slide>
-        <div class="slide">
-          <img src="/assets/images/normal_u83.svg"/>
-          <p>You don’t have to search and type addresses</p>
-        </div>
-        </ion-slide>
-
-        <ion-slide>
-        <div class="slide">
-          <img src="/assets/images/normal_u95.svg"/>
-          <p>You don’t have to print paper #green</p>
-        </div>
-        </ion-slide>
-
-        <ion-slide>
-        <div class="slide">
-          <img src="/assets/images/normal_u107.svg"/>
-          <p>You don’t have to buy envelope and stamps</p>
-        </div>
-        </ion-slide>
-
-      </ion-slides>
-
-        <ion-row>
-        <ion-col size="12">
-          <ion-button type="submit" shape="round" class="get_st" @click="() => router.push('/all-letters')">Get Started</ion-button>
-        </ion-col>
-      </ion-row>
-       
-
       </div>
     </ion-content>
   </ion-page>
@@ -69,62 +70,62 @@
 <ion-page scroll-y="false">
     <ion-content fullscreen="true">
       <div id="container_intro" class="main_self">
-      <ion-row>
-        <ion-col size="12"> 
-        <div class="drak_mode">
-          <img class="w-100" src="assets/images/dark_mode_logo.svg"/>
-        </div>
-          <div class="logo">
-            <img src="/assets/images/normal_u1.svg">
+        <div class="intro-conty">
+          <ion-row>
+          <ion-col size="12"> 
+          <div class="drak_mode">
+              <img class="logo-dark" src="assets/images/dark_mode_logo.svg"/>
           </div>
-        </ion-col>
-      </ion-row>
-      
-        <ion-slides pager="true">
-        <ion-slide>
+            <div class="logo">
+              <img src="/assets/images/normal_u1.svg">
+            </div>
+          </ion-col>
+        </ion-row>
+        
+          <ion-slides pager="true">
+          <ion-slide>
+            <div class="slide">
+              <img src="/assets/images/normal_u61.svg"/>
+              <p>This app allows you to write letters & faxes easily</p>
+            </div>
+          </ion-slide>
+
+          <ion-slide>
           <div class="slide">
-            <img src="/assets/images/normal_u61.svg"/>
-            <p>This app allows you to write letters & faxes easily</p>
+            <img src="/assets/images/normal_u71.svg"/>
+            <p>You don’t have to format nicely, we do that for you</p>
           </div>
-        </ion-slide>
+          </ion-slide>
 
-        <ion-slide>
-        <div class="slide">
-          <img src="/assets/images/normal_u71.svg"/>
-          <p>You don’t have to format nicely, we do that for you</p>
+          <ion-slide>
+          <div class="slide">
+            <img src="/assets/images/normal_u83.svg"/>
+            <p>You don’t have to search and type addresses</p>
+          </div>
+          </ion-slide>
+
+          <ion-slide>
+          <div class="slide">
+            <img src="/assets/images/normal_u95.svg"/>
+            <p>You don’t have to print paper #green</p>
+          </div>
+          </ion-slide>
+
+          <ion-slide>
+          <div class="slide">
+            <img src="/assets/images/normal_u107.svg"/>
+            <p>You don’t have to buy envelope and stamps</p>
+          </div>
+          </ion-slide>
+
+        </ion-slides>
+
+          <ion-row>
+          <ion-col size="12">
+            <ion-button type="submit" shape="round" class="get_st" @click="() => router.push('/all-letters')">Get Started</ion-button>
+          </ion-col>
+        </ion-row>
         </div>
-        </ion-slide>
-
-        <ion-slide>
-        <div class="slide">
-          <img src="/assets/images/normal_u83.svg"/>
-          <p>You don’t have to search and type addresses</p>
-        </div>
-        </ion-slide>
-
-        <ion-slide>
-        <div class="slide">
-          <img src="/assets/images/normal_u95.svg"/>
-          <p>You don’t have to print paper #green</p>
-        </div>
-        </ion-slide>
-
-        <ion-slide>
-        <div class="slide">
-          <img src="/assets/images/normal_u107.svg"/>
-          <p>You don’t have to buy envelope and stamps</p>
-        </div>
-        </ion-slide>
-
-      </ion-slides>
-
-        <ion-row>
-        <ion-col size="12">
-          <ion-button type="submit" shape="round" class="get_st" @click="() => router.push('/all-letters')">Get Started</ion-button>
-        </ion-col>
-      </ion-row>
-       
-
       </div>
     </ion-content>
 </ion-page>
